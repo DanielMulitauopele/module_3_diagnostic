782cac149e6c08ec6acddb3c2ad3262ca05f0423
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-17 11:03:39 -0600

    refactoring so that service is separate from the search, adjusting so that any search params can entered in to the search

--- app/models/station_search.rb
+++ app/models/station_search.rb
@@ -1,11 +1,13 @@
 class StationSearch
   def stations
-    conn = Faraday.new(url: ENV['ROOT_URL'])
-
-    results = JSON.parse(conn.get("/api/alt-fuel-stations/v1/nearest.json?location=80203&limit=10&fuel_type=E85,ELEC&api_key=#{ENV['API_KEY']}").body, symbolize_names: true)
-
-    @stations = results[:fuel_stations].map do |station_info|
+    @stations ||= service.get_stations[:fuel_stations].map do |station_info|
       Station.new(station_info)
     end
+  end
+
+  private
+
+  def service
+    NrelService.new
   end
 end
---
+++ app/services/nrel_service.rb
@@ -0,0 +1,15 @@
+class NrelService
+  def get_stations
+    get_json("/api/alt-fuel-stations/v1/nearest.json?location=80203&limit=10&fuel_type=E85,ELEC&api_key=#{ENV['API_KEY']}")
+  end
+
+  private
+
+  def conn
+    conn = Faraday.new(url: ENV['ROOT_URL'])
+  end
+
+  def get_json(url)
+    JSON.parse(conn.get(url).body, symbolize_names: true)
+  end
+end
