119de30296adca2f2c7590a43b9b57893753f941
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-17 10:17:03 -0600

    not much done on the stations search, i think im getting hung up on the fact that my request is not returning what i think it should be returning, gonna build out the infrastructure anyways

--- app/models/station_search.rb
+++ app/models/station_search.rb
@@ -1,7 +1,8 @@
 class StationSearch
   def stations
     conn = Faraday.new(url: ENV['ROOT_URL'])
-    response = JSON.parse(conn.get("/api/alt-fuel-stations/v1.json?zip=80203&api_key=#{ENV["API_KEY"]}").body, symbolize_names: true)
-    require "pry"; binding.pry
+
+    results = JSON.parse(conn.get("/api/alt-fuel-stations/v1.json?zip=80203&api_key=#{ENV["API_KEY"]}").body, symbolize_names: true)
+    results[:]
   end
 end
