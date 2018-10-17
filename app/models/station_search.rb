0a6c061b50537eab90fdec8d132a7dc6d9625c74
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2018-10-17 10:06:54 -0600

    got request to pass through, using ENV variables to fill in for sensitive data, still only returning 4 station as far as I can tell

--- app/models/station_search.rb
+++ app/models/station_search.rb
@@ -1,7 +1,7 @@
 class StationSearch
   def stations
     conn = Faraday.new(url: ENV['ROOT_URL'])
-    response = JSON.parse(conn.get("zip=80203&api_key=#{ENV['API_KEY']}").body, symbolize_names: true)
+    response = JSON.parse(conn.get("/api/alt-fuel-stations/v1.json?zip=80203&api_key=#{ENV["API_KEY"]}").body, symbolize_names: true)
     require "pry"; binding.pry
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,13 +5,13 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'webmock/rspec'
-require 'vcr'
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'fixtures/vcr_cassettes'
-  config.hook_into :webmock
-end
+# require 'webmock/rspec'
+# require 'vcr'
+#
+# VCR.configure do |config|
+#   config.cassette_library_dir = 'fixtures/vcr_cassettes'
+#   config.hook_into :webmock
+# end
 
 SimpleCov.start "rails"
 # Add additional requires below this line. Rails is not loaded until this point!
